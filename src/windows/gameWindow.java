f3d962c2a8f9fc01684711ea8575b6c0bbbfb726
Bui Huy <huy.bui91221@gmail.com>
2020-01-03 21:14:55 +0700

    Design GUI & Add resources

--- src/windows/gameWindow.java
+++ src/windows/gameWindow.java
@@ -1,24 +1,35 @@
 package windows;
 
-import java.awt.EventQueue;
+//import java.awt.EventQueue;
 
+import troops.*;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import java.awt.Panel;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 
 public class gameWindow {
 
-	private JFrame frame;
+	 JFrame frame;
+	private ImageIcon iconbackground;
 
 	/**
 	 * Launch the application.
-	 */
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					gameWindow window = new gameWindow();
+					gameWindow window = new gameWindow(1);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -26,12 +37,23 @@ public class gameWindow {
 			}
 		});
 	}
-
-	/**
+	
 	 * Create the application.
 	 */
-	public gameWindow() {
+	public gameWindow(int i) {
+		switch (i) {
+		case 1:
+			iconbackground= new ImageIcon("src/resources/GrassTerrain.png");
+			break;
+		case 2:
+			iconbackground= new ImageIcon("src/resources/IceTerrain.png");
+			break;
+		case 3:
+			iconbackground= new ImageIcon("src/resources/DessertTerrain.png");
+			break;
+		}
 		initialize();
+		this.frame.setVisible(true);
 	}
 
 	/**
@@ -39,9 +61,32 @@ public class gameWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 900, 700);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		frame.setBounds(100, 100, 1200, 645);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lbbackground = new JLabel(iconbackground);
+		lbbackground.setBounds(0, 0, 768, 480);
+		frame.getContentPane().add(lbbackground);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(926, 10, 260, 190);
+		TitledBorder border=BorderFactory.createTitledBorder("Player 1");
+		panel.setBorder(border);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("End Turn");
+		btnNewButton.setBounds(13, 25, 235, 68);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Surrender");
+		btnNewButton_1.setBounds(13, 109, 235, 68);
+		panel.add(btnNewButton_1);
+		
+		JPanel infopanel = new JPanel();
+		infopanel.setBounds(926, 210, 260, 399);
+		infopanel.setBorder(BorderFactory.createTitledBorder("Information"));
+		frame.getContentPane().add(infopanel);
 	}
-
 }
---
+++ src/windows/menuWindow.java
@@ -0,0 +1,144 @@
+package windows;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class menuWindow {
+
+	private JFrame frame;
+	gameWindow game;
+	WindowAdapter onGameClosed;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					menuWindow window = new menuWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public menuWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1280, 700);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnNewGame = new JButton("New Game");
+		btnNewGame.setBounds(522, 375, 200, 70);
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().add(btnNewGame);
+		
+		JButton btnContinue = new JButton("Continue");
+		btnContinue.setBounds(522, 470, 200, 70);
+		btnContinue.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		frame.getContentPane().add(btnContinue);
+		
+		JButton btnHelp = new JButton("Help");
+		btnHelp.setBounds(522, 565, 200, 70);
+		btnHelp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		frame.getContentPane().add(btnHelp);
+		
+		JButton btnGrass = new JButton("Grass");
+		btnGrass.setBounds(522, 375, 200, 70);
+		btnGrass.setVisible(false);
+		btnGrass.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				game=new gameWindow(1);
+				frame.setVisible(false);
+				game.frame.addWindowListener(onGameClosed);
+			}
+		});
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().add(btnGrass);
+		
+		JButton btnSnow = new JButton("Snow");
+		btnSnow.setBounds(522, 470, 200, 70);
+		btnSnow.setVisible(false);
+		btnSnow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				game=new gameWindow(2);
+				frame.setVisible(false);
+				game.frame.addWindowListener(onGameClosed);
+			}
+		});
+		frame.getContentPane().add(btnSnow);
+		
+		JButton btnDesert = new JButton("Desert");
+		btnDesert.setBounds(522, 565, 200, 70);
+		btnDesert.setVisible(false);
+		btnDesert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				game=new gameWindow(3);
+				frame.setVisible(false);
+				game.frame.addWindowListener(onGameClosed);
+			}
+		});
+		frame.getContentPane().add(btnDesert);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(124, 12, 1000, 300);
+		frame.getContentPane().add(panel);
+		
+		ImageIcon titleicon = new ImageIcon("src/resources/Title.png", "Medieval War Title");
+		JLabel title = new JLabel(titleicon);
+		panel.add(title);
+		
+		//ActionListener for btnNewGame
+		btnNewGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnNewGame.setVisible(false);
+				btnContinue.setVisible(false);
+				btnHelp.setVisible(false);
+				btnGrass.setVisible(true);
+				btnSnow.setVisible(true);
+				btnDesert.setVisible(true);
+			}
+		});
+		
+		onGameClosed=new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				btnNewGame.setVisible(true);
+				btnContinue.setVisible(true);
+				btnHelp.setVisible(true);
+				btnGrass.setVisible(false);
+				btnSnow.setVisible(false);
+				btnDesert.setVisible(false);
+				frame.setVisible(true);
+			}
+		};
+	}
+}
